36517d0e012a8a953fdb0d6e56a2ae2aeada16e8
Reid <rmcc0724@gmail.com>
2019-07-12 14:30:09 -0700

    Final Touches

--- js/team.js
+++ js/team.js
@@ -1,55 +1,55 @@
 // JavaScript File
 
 
-var html2 = "<h1>Check out our team of Web Developers</h1>";
+var html2 = "<h1>Our team of Web Developers</h1>";
 
 html2 +=    "<ul id=gallery>";
 html2 +=    "<li>";
-html2 +=    "<a href=http://www.girlsingerjoyce.com>";
-html2 +=    "<img src=images/joyce.png alt=Aaron Su>";
+html2 +=    "<a target=_blank href=https://codepen.io/asutcscu/pen/qzLYqY?editors=1100>";
+html2 +=    "<img src=img/aaron.jpg alt=Aaron Su>";
 html2 +=    "<p>Aaron Su</p>";
 html2 +=    "</a>";
 html2 +=    "</li>";
 
 html2 +=    "<li>";
-html2 +=    "<a href=https://rmcc0724.github.io/portfolio/nasa_project/index.html>";
-html2 +=    "<img src=images/nasa_portfolio.png alt=Florence Le>";
+html2 +=    "<a target=_blank href=https://codepen.io/Florence3524/pen/ewbVqw>";
+html2 +=    "<img src=img/Florence.png alt=Florence Le>";
 html2 +=    "<p>Florence Le</p>";
 html2 +=    "</a>";
 html2 +=    "</li>";
 
 html2 +=    "<li>";
-html2 +=    "<a href=https://lulf.org>";
-html2 +=    "<img src=images/lulf.png alt=Lucas Chua>";
+html2 +=    "<a target=_blank href=https://codepen.io/FIJI/pen/YodeRG>";
+html2 +=    "<img src=img/LUCAS.jpg alt=Lucas Chua>";
 html2 +=    "<p>Lucas Chua</p>";
 html2 +=    "</a>";
 html2 +=    "</li>";
 
 
 html2 +=    "<li>";
-html2 +=    "<a href=https://rmcc0724.github.io/templates>";
-html2 +=    "<img src=images/apple.png alt=Minghui Zhao>";
+html2 +=    "<a target=_blank href=https://codepen.io/mzhao123/pen/dBQOMY?editors=1000>";
+html2 +=    "<img src=img/mingz.jpg alt=Minghui Zhao>";
 html2 +=    "<p>Minghui Zhao</p>";
 html2 +=    "</a>";
 html2 +=    "</li>";
 
 html2 +=    "<li>";
-html2 +=    "<a href=google/index.html>";
-html2 +=    "<img src=images/google-mockup-final.png alt=Miro Durana>";
+html2 +=    "<a target=_blank href=https://codepen.io/akutasan/project/full/XRKwnE>";
+html2 +=    "<img src=img/Miro2.jpg alt=Miro Durana>";
 html2 +=    "<p>Miro Durana</p>";
 html2 +=    "</a>";
 html2 +=    "</li>";
 
 html2 +=    "<li>";
-html2 +=    "<a href=http://newsroom.mgm.mo>";
-html2 +=    "<img src=images/mgm2.png alt=Nicholas Hu>";
+html2 +=    "<a target=_blank href=https://codepen.io/Nich0las/pen/OerQez>";
+html2 +=    "<img src=img/Nicholas.jpg alt=Nicholas Hu>";
 html2 +=    "<p>Nicholas Hu</p>";
 html2 +=    "</a>";
 html2 +=    "</li>";
 
 html2 +=    "<li>";
-html2 +=    "<a href=http://news.keuriggreenmountain.com>";
-html2 +=    "<img src=images/keurig.png alt=Sofia Tedesco>";
+html2 +=    "<a target=_blank href=https://codepen.io/sofiayf/pen/VJNxGy>";
+html2 +=    "<img src=img/Sofia.jpg alt=Sofia Tedesco>";
 html2 +=    "<p>Sofia Tedesco</p>";
 html2 +=    "</a>";
 html2 +=    "</li>";
