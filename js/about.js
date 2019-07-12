9fec3ba0dee24cd403473cd5e1d2016fbb5db088
Reid <rmcc0724@gmail.com>
2019-07-11 19:08:06 -0700

    first commit

---
+++ js/about.js
@@ -0,0 +1,8 @@
+// JavaScript File
+var html1="<section id=about><img class=profile-pic src=img/IMG_0513.JPG>";
+html1 +="<h3>Welcome to the 2019 WebRox Landing Page</h3>";
+html1 +="<p>Hello I'm Reid McCulloch and this is the landing page for the 2019 WebRox web development bootcamp. "; 
+html1 +="Our new team of developers went through intense training to learn the foundations of Web Development such as HTML, CSS, JavaScript and JQuery.</p>";
+html1 +="<p>Throughout the duration of the camp they've been working on their own projects using all of the tools they've learned and I'm happy to present you each of their projects by clicking the button below</p>";
+html1 +=`<div class=team-btn><button onclick="myFunction(this, 'selected', 2)">Check out our team</button></div>`;
+html1+="</section>"
---
+++ js/team.js
@@ -0,0 +1,58 @@
+// JavaScript File
+
+
+var html2 = "<h1>Check out our team of Web Developers</h1>";
+
+html2 +=    "<ul id=gallery>";
+html2 +=    "<li>";
+html2 +=    "<a href=http://www.girlsingerjoyce.com>";
+html2 +=    "<img src=images/joyce.png alt=Aaron Su>";
+html2 +=    "<p>Aaron Su</p>";
+html2 +=    "</a>";
+html2 +=    "</li>";
+
+html2 +=    "<li>";
+html2 +=    "<a href=https://rmcc0724.github.io/portfolio/nasa_project/index.html>";
+html2 +=    "<img src=images/nasa_portfolio.png alt=Florence Le>";
+html2 +=    "<p>Florence Le</p>";
+html2 +=    "</a>";
+html2 +=    "</li>";
+
+html2 +=    "<li>";
+html2 +=    "<a href=https://lulf.org>";
+html2 +=    "<img src=images/lulf.png alt=Lucas Chua>";
+html2 +=    "<p>Lucas Chua</p>";
+html2 +=    "</a>";
+html2 +=    "</li>";
+
+
+html2 +=    "<li>";
+html2 +=    "<a href=https://rmcc0724.github.io/templates>";
+html2 +=    "<img src=images/apple.png alt=Minghui Zhao>";
+html2 +=    "<p>Minghui Zhao</p>";
+html2 +=    "</a>";
+html2 +=    "</li>";
+
+html2 +=    "<li>";
+html2 +=    "<a href=google/index.html>";
+html2 +=    "<img src=images/google-mockup-final.png alt=Miro Durana>";
+html2 +=    "<p>Miro Durana</p>";
+html2 +=    "</a>";
+html2 +=    "</li>";
+
+html2 +=    "<li>";
+html2 +=    "<a href=http://newsroom.mgm.mo>";
+html2 +=    "<img src=images/mgm2.png alt=Nicholas Hu>";
+html2 +=    "<p>Nicholas Hu</p>";
+html2 +=    "</a>";
+html2 +=    "</li>";
+
+html2 +=    "<li>";
+html2 +=    "<a href=http://news.keuriggreenmountain.com>";
+html2 +=    "<img src=images/keurig.png alt=Sofia Tedesco>";
+html2 +=    "<p>Sofia Tedesco</p>";
+html2 +=    "</a>";
+html2 +=    "</li>";
+
+
+html2 +=    "</ul>";
